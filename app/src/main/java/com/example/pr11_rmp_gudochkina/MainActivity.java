d3d08a7e1d3275954f769581db30f3b626e13ca8
1qwe-qwe1 <qudasetl@gmail.com>
2025-12-17 21:36:36 +0700

    added stars

--- app/src/main/java/com/example/pr11_rmp_gudochkina/MainActivity.java
+++ app/src/main/java/com/example/pr11_rmp_gudochkina/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.pr11_rmp_gudochkina;
 
+import android.animation.ObjectAnimator;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -32,5 +30,37 @@ public class MainActivity extends AppCompatActivity {
         ImageView hourImageView = findViewById(R.id.hour_hand);
         Animation hourTurnAnimation = AnimationUtils.loadAnimation(this, R.anim.hour_turn);
         hourImageView.startAnimation(hourTurnAnimation);
+        animateStars();
+
+        brightenSkyBackground();
+    }
+    private void animateStars() {
+        int[] starIds = {R.id.star1, R.id.star2, R.id.star3, R.id.star4, R.id.star5};
+
+        for (int starId : starIds) {
+            ImageView star = findViewById(starId);
+            Animation starAnimation = AnimationUtils.loadAnimation(this, R.anim.stars_disappear);
+            starAnimation.setStartOffset((int) (Math.random() * 1000));
+            star.startAnimation(starAnimation);
+        }
+    }
+
+    private void brightenSkyBackground() {
+        ImageView skyImageView = findViewById(R.id.sky);
+
+        LayerDrawable layerDrawable = (LayerDrawable) skyImageView.getDrawable();
+        if (layerDrawable != null) {
+            Drawable nightSky = layerDrawable.getDrawable(1);
+
+            ObjectAnimator fadeOutAnimator = ObjectAnimator.ofInt(
+                    nightSky,
+                    "alpha",
+                    255,
+                    0
+            );
+            fadeOutAnimator.setDuration(5000);
+            fadeOutAnimator.start();
+        }
+
     }
 }
